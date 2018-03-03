9a01f920bf85855394534949fd9e405892e988ec
Wojciech Figas <wojciech.figas@outlook.com>
2018-03-03 22:05:01 +0100

    add basic validation

--- tutoroservice/src/main/java/com/tutoro/dto/UserDto.java
+++ tutoroservice/src/main/java/com/tutoro/dto/UserDto.java
@@ -1,10 +1,22 @@
 package com.tutoro.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class UserDto {
+    @NotNull
+    @Min(5)
     private String username;
+
+    @NotNull
+    @Min(5)
     private String password;
+
+    @Email
+    @NotNull
     private String email;
 }
