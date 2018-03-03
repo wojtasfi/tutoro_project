f5ab167924ffbc56bce4ab52b6f8fc1f81e6e7ea
Wojciech Figas <wojciech.figas@outlook.com>
2018-03-03 20:17:41 +0100

    show swagger when in default mode

--- graph-relation-service/src/main/java/com/relationservice/config/ResourceServerProductionConfiguration.java
+++ graph-relation-service/src/main/java/com/relationservice/config/ResourceServerProductionConfiguration.java
@@ -1,17 +1,19 @@
 package com.relationservice.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 
 @Configuration
-public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+@Profile("production")
+public class ResourceServerProductionConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/env", "/info")
+                .antMatchers("/", "/env", "/info", "/swagger-resources/**", "/swagger-ui/index.html")
                 .permitAll()
                 .antMatchers("/relations/**")
                 .hasRole("TUTOR")
--- tutoroservice/src/main/java/com/tutoro/config/ResourceServerProductionConfiguration.java
+++ tutoroservice/src/main/java/com/tutoro/config/ResourceServerProductionConfiguration.java
@@ -1,19 +1,22 @@
 package com.tutoro.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 
 @Configuration
-public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+@Profile("production")
+public class ResourceServerProductionConfiguration extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
 
         http.antMatcher("/**")
                 .authorizeRequests()
                 .antMatchers("/", "/env", "/info", "/users/register",
-                        "/users/validate/username", "/users/validate/email", "users/verify/email")
+                        "/users/validate/username", "/users/validate/email", "users/verify/email",
+                        "/swagger-ui.html", "/swagger-ui/index.html", "/swagger-resources/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
