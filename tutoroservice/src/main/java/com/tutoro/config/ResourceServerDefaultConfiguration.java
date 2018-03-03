1ecd4a56f75d0b470eeb4097428e523a9216b2bd
Wojciech Figas <wojciech.figas@outlook.com>
2018-03-03 20:16:50 +0100

    show swagger when in default mode

---
+++ tutoroservice/src/main/java/com/tutoro/config/ResourceServerDefaultConfiguration.java
@@ -0,0 +1,19 @@
+package com.tutoro.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@Profile("default")
+public class ResourceServerDefaultConfiguration extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+
+    }
+}
